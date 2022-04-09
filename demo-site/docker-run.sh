83f9d5c453fcbf66981521b42f9acbefa9de793e
Matt Schiller <mschiller@banyansecurity.io>
2022-04-09 13:43:38 -0700

    show hostname of server

--- demo-site/docker-run.sh
+++ demo-site/docker-run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-
+echo "let name = \"$HOSTNAME\";" > /html/env.js
+echo "document.write(name);" >> /html/env.js
 docker run -v /html:/usr/share/nginx/html:ro -p 80:80 nginx:1.20.2
--- demo-site/html/env.sh
+++
@@ -1,3 +0,0 @@
-#! bin/bash
-echo "let name = \"$HOSTNAME\";" > /html/env.js
-echo "document.write(name);" >> /html/env.js
