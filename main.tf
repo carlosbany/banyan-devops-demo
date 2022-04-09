04d265ff49e875bb2024bd128b11b85354764bbf
Matt Schiller <mschiller@banyansecurity.io>
2022-04-09 11:23:00 -0700

    update api

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ locals {
   connector_api_key = ""
   banyan_host = "https://team.console.banyanops.com/"
   key_name   = "matt"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCfrFZbCxqMF4wdro+dIfg/MEpqhdByyS8+ou2sLSWkg7iIgOdz4N8LRR2hIXFR9AfnvscBMndMa0KgiixGqwmdJk25W2P0eOmdsTCNOmyA3z7xt1G9+RflzKv0cNWnjF6UtinC+WHOaSBRCt1Td7zD8oLjH0GGoWEpUB7p5MsbLgv/H3VzVejOfuPnO4/JB6RYdFSZSvIOFrzyhnaMMG+7dyyBmtF3r4lfeOuzW/vkBtpRjeh3Ocju61pid0c0KdZPx7MsMPqnrooBFwRhTSPwz1/vsoeQc5eX66bqOt2efJjIcTsRP9ynZeA2zbbClNe8dHPybdQhtNJOOXeRIgoJy5E8DZoYmsjybNyEHpFkouM2Fk7Ni/bLnesvOYqAquNpt02n2cWGT2M3oMNBrTOcsiSXAQIpB80PNlQGgIYLWWtcgcO8pkFx4bFFuE23lIuDVeHHxGsm0raes4lYiQGKDfXJC87Nz3pGvFvDsdFiXB3Wma38Wh3T3TpPfeF3f7c= matt@moviess-MacBook-Pro.local"
+  public_key = ""
 }
 
 terraform {
